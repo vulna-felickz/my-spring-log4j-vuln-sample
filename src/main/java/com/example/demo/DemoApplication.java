6ccb69d93c92b59039f2d3bcf4575c0d2700fc7e
Chad Bentz <1760475+felickz@users.noreply.github.com>
2021-12-21 15:20:06 -0500

    intro vuln

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		//final Logger logger = LogManager.getLogger(DemoApplication.class);
+		final Logger logger = LogManager.getLogger(DemoApplication.class);
 		
 		SpringApplication.run(DemoApplication.class, args);
 
